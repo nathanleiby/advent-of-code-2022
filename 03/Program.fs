198f0fe1660a5a606e21ee14c8335686d1ebd93f
Nathan Leiby <nathanleiby@gmail.com>
2022-12-03 18:15:31 -0800

    day 3 (part 2)

--- 03/Program.fs
+++ 03/Program.fs
@@ -1,8 +1,4 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#!"
-
-open System.IO
-open System.Collections.Generic
+﻿open System.IO
 
 // utils
 // copied from https://github.com/jack-pappas/ExtCore/blob/2adff09ca1ed6a555a8e6564203ec8dbfeeb76c8/ExtCore/Pervasive.fs#L210-L212
@@ -10,8 +6,9 @@ let tap (action: 'T -> unit) (value: 'T) : 'T =
     action value
     value
 
+let tee (value: 'T) = tap (fun x -> printfn "%A" x) value
 
-let splitList (list) =
+let splitListInHalf (list) =
     let n = (Seq.length list) / 2
     let firstList = list |> Seq.take n |> Seq.toList
     let secondList = list |> Seq.skip n |> Seq.toList
@@ -29,7 +26,7 @@ let letterScore c =
 
 let processLine line =
     // split in half
-    splitList line
+    splitListInHalf line
     // convert to Set
     // |> tap (fun x -> printfn "line =  %A" x)
     |> (fun pair -> (Set.ofList (fst pair), Set.ofList (snd pair)))
@@ -47,5 +44,34 @@ let partOne file =
     |> Seq.sum
     |> printfn "%A"
 
+// Intersects three sets and computes letter score of the output
+let partTwoHelper seq =
+    let l = List.ofSeq seq
+
+    let i1 = Set.intersect l[0] l[1]
+    let i2 = Set.intersect i1 l[2]
+
+    i2 |> Set.toList |> (fun x -> x[0]) |> letterScore
+
+
+let seqToSet x = x |> Seq.toList |> Set.ofList
+
+let partTwo file =
+    File.ReadAllLines(file)
+    |> Array.chunkBySize 3
+    |> Seq.ofArray
+    // |> tee
+    |> Seq.map (Seq.map seqToSet)
+    // |> tee
+    |> Seq.map partTwoHelper
+    // |> tee
+    |> Seq.sum
+    |> printfn "%A"
+
+printfn "Part One:"
 partOne "./example.txt"
 partOne "./input.txt"
+
+printfn "Part Two:"
+partTwo "./example.txt"
+partTwo "./input.txt"
