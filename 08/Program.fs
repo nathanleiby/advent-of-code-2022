e8c318274db0eb42c2cd66213ad380153b20e821
Nathan Leiby <nathanleiby@gmail.com>
2022-12-09 23:53:40 -0800

    day 8: part 2

--- 08/Program.fs
+++ 08/Program.fs
@@ -6,8 +6,6 @@ let parseInput f =
     File.ReadAllLines f |> Seq.map parseLine |> Array.ofSeq
 
 let isVisible (rowIdx: int, colIdx: int, matrix: int array array) =
-    // from left?
-
     let height = matrix[rowIdx][colIdx]
     let matrixWidth = Array.length matrix[rowIdx]
     let matrixHeight = Array.length matrix
@@ -51,9 +49,84 @@ let partOne f =
 
     total
 
+let computeScenicScore (rowIdx: int, colIdx: int, matrix: int array array) =
+    let height = matrix[rowIdx][colIdx]
+    let matrixWidth = Array.length matrix[rowIdx]
+    let matrixHeight = Array.length matrix
+
+    let mutable isDone = false
+
+    // left
+    let mutable leftScore = 0
+    isDone <- false
+
+    for i = (colIdx - 1) downto 0 do
+        if (not isDone) then
+            leftScore <- leftScore + 1
+
+        if (matrix[rowIdx][i] >= height) then
+            isDone <- true
+
+    // right
+    let mutable rightScore = 0
+    isDone <- false
+
+    for i = (colIdx + 1) to (matrixWidth - 1) do
+        if (not isDone) then
+            rightScore <- rightScore + 1
+
+        if (matrix[rowIdx][i] >= height) then
+            isDone <- true
+
+    // top
+    let mutable topScore = 0
+    isDone <- false
+
+    for i = (rowIdx - 1) downto 0 do
+        if (not isDone) then
+            topScore <- topScore + 1
+
+        if (matrix[i][colIdx] >= height) then
+            isDone <- true
+
+    // bot
+    let mutable botScore = 0
+    isDone <- false
+
+    for i = (rowIdx + 1) to (matrixHeight - 1) do
+        if (not isDone) then
+            botScore <- botScore + 1
+
+        if (matrix[i][colIdx] >= height) then
+            isDone <- true
+
+    leftScore * rightScore * topScore * botScore
+
+
+let partTwo f =
+    let matrix = parseInput f
+
+    let mutable bestScore = 0
+
+    for rowIdx, row in Array.indexed matrix do
+        for colIdx, _ in Array.indexed row do
+            let newScore = computeScenicScore (rowIdx, colIdx, matrix)
+
+            if (newScore > bestScore) then
+                bestScore <- newScore
+
+    bestScore
+
 printfn "== Part one =="
 let example = partOne "./example.txt"
 printfn "Example = %d" example
 assert (example = 21)
 let input = partOne "./input.txt"
 printfn "Input = %d" input
+
+printfn "== Part two =="
+let example2 = partTwo "./example.txt"
+printfn "Example = %d" example2
+assert (example2 = 8)
+let input2 = partTwo "./input.txt"
+printfn "Input = %d" input2
